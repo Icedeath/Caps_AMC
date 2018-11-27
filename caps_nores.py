141f105694d9feebd11733ab48420679550eb52e
Icedeath <2724432@qq.com>
2018-11-27 16:06:26 +0000

    label prediction fixed

--- caps_nores.py
+++ caps_nores.py
@@ -5,7 +5,7 @@ import numpy as np
 from keras import layers, models, optimizers
 from keras import backend as K
 from keras.layers import Lambda
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from capsulelayers2 import CapsuleLayer, PrimaryCap, Length, Mask
 from keras import callbacks
@@ -50,6 +50,9 @@ def CapsNet(input_shape, n_class, routings):
     conv1 = layers.Conv2D(filters=192, kernel_size=(1,3), strides=1, padding='same')(conv1)
     conv1 = ELU(alpha=0.5)(conv1)
     conv1 = BN()(conv1)
+    conv1 = layers.Conv2D(filters=192, kernel_size=(1,3), strides=1, padding='same')(conv1)
+    conv1 = ELU(alpha=0.5)(conv1)
+    conv1 = BN()(conv1)
     conv1 = layers.MaxPooling2D((1, 2), strides=(1, 2))(conv1)
 
     conv1 = layers.Conv2D(filters=256, kernel_size=(1,3), strides=1, padding='same')(conv1)
@@ -58,7 +61,10 @@ def CapsNet(input_shape, n_class, routings):
     conv1 = layers.Conv2D(filters=256, kernel_size=(1,3), strides=1, padding='same')(conv1)
     conv1 = ELU(alpha=0.5)(conv1)
     conv1 = BN()(conv1)
-
+    conv1 = layers.Conv2D(filters=256, kernel_size=(1,3), strides=1, padding='same')(conv1)
+    conv1 = ELU(alpha=0.5)(conv1)
+    conv1 = BN()(conv1)
+    
     primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=(1,3),
                              strides=1, padding='same')
     digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=args.dim_capsule, routings=routings,
@@ -83,7 +89,7 @@ def margin_loss(y_true, y_pred, margin = 0.4, downweight = 0.5):
 def train(model, data, args):
     (x_train, y_train) = data
 
-    checkpoint = callbacks.ModelCheckpoint(args.save_file, monitor='loss', verbose=1, save_best_only=True, 
+    checkpoint = callbacks.ModelCheckpoint(args.save_file, monitor='val_loss', verbose=1, save_best_only=True, 
                                   save_weights_only=True, mode='auto', period=1)
     lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
     #model = multi_gpu_model(model, gpus=2)
@@ -112,16 +118,17 @@ if __name__ == "__main__":
                         help="Number of iterations used in routing algorithm. should > 0")
     parser.add_argument('-sf', '--save_file', default='0_15_3500_16.h5',
                         help="Name of saved weight file")
-    parser.add_argument('-t', '--test', default=0,type=int,
+    parser.add_argument('-t', '--test', default=1,type=int,
                         help="Test only model")
-    parser.add_argument('-l', '--load', default=0,type=int,
+    parser.add_argument('-l', '--load', default=1,type=int,
                         help="load weight file or not")
     parser.add_argument('-p', '--plot', default=0,type=int,
                         help="plot training loss after finished if plot==1")
     parser.add_argument('-d', '--dataset', default='./samples/dataset_MAMC_10.mat',
                         help="name of dataset that needs loading")
     parser.add_argument('-n', '--num_classes', default=10)
-    parser.add_argument('-dc', '--dim_capsule', default=20)
+    parser.add_argument('-dc', '--dim_capsule', default=16)
+    parser.add_argument('-tm', '--target_max', default=2, type=int)
     args = parser.parse_args()
     print(args)
     
@@ -164,10 +171,26 @@ if __name__ == "__main__":
         print('Loading %s' %args.save_file)
       
     print('-'*30 + 'Begin: test' + '-'*30)
-    y_pred = model.predict(x_train, batch_size=args.batch_size,verbose=1)
-    y_pred = np.sign(np.reshape(y_pred, np.prod(y_pred.shape)))
-    y = np.squeeze(np.reshape(y_train, np.prod(y_train.shape)))
-    print('Train acc:', np.sum(y_pred == y)/np.float(y_train1.shape[0])/2)
+    y_pred1 = model.predict(x_train, batch_size=args.batch_size,verbose=1)
+    y_pred1 = (np.sign(y_pred1-0.5)+1)/2
+    idx_yt = np.sum(y_train, axis = 1)
+    idx_yp = np.sum(y_pred1, axis = 1)
+    idx_mc = []
+    y_pred = np.zeros([y_pred1.shape[0],args.target_max])
+    y_true = y_pred
+    for i in xrange(y_pred.shape[0]):
+        if np.mod(i,2000)==0:
+            print(i)
+        _, y_t = tf.nn.top_k(y_train, idx_yt[i])
+        _, y_p = tf.nn.top_k(y_pred1, idx_yt[i])
+        y_t = K.eval(y_t)
+        y_t.sort(axis = 0)
+        y_p = K.eval(y_p)
+        y_p.sort(axis = 0)
+        y_true[i,0:idx_yt[i]] = y_t
+        y_pred[i,0:idx_yp[i]] = y_p
+        
+    #print('Train acc:', np.sum(y_pred1_tr == y_1)/np.float(y_1.shape[0]))
     print('-' * 30 + 'End: test' + '-' * 30)   
 
     
