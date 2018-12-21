79315df4d363bd1e121f91cf20740996735228c9
Icedeath <2724432@qq.com>
2018-12-21 15:54:56 +0000

    cm updated

--- confusion.m
+++ confusion.m
@@ -1,11 +1,11 @@
 clear all
 load acc
 
-cm(:,9) = cm(:,9)/(sum(sum((cm(1:8,1:8))))+sum(cm(:,9)));
-cm(9,:) = cm(9,:)/(sum(sum((cm(1:8,1:8))))+sum(cm(9,:)));
+cm(:,9) = cm(:,9)/(sum(sum((cm(1:8,1:8))))+sum(cm(:,9)))*8;
+%cm(9,:) = cm(9,:)/(sum(sum((cm(1:8,1:8))))+sum(cm(9,:)));
 for i=1:8
-    a=sum(cm(1:8,i));
-    cm(1:8,i) = cm(1:8,i)/a;
+    a=sum(cm(:,i));
+    cm(:,i) = cm(:,i)/a;
 end
 
 
